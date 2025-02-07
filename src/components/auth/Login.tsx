654c6d0c33f88a6958bad66445352aecbea919f1
Devansh-Walia <devanshwalia9898@gmail.com>
2025-02-07 11:44:48 +0530

    remove console log

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -15,8 +15,6 @@ const Login: React.FC = () => {
     }
   };
 
-  console.log(appConfig);
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
